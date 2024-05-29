f943086d9d9dbe263ef1de107a631dd4370c956a
JayHung <youngjay9@gmail.com>
2024-05-29 12:42:15 +0800

    新增 component: BaseCard, 提供 slot 的功能.

---
+++ vite-project/src/components/BaseCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+    <div class="title">{{ title }}</div>
+    <div class="content">
+      <slot></slot>
+      <!-- <slot>
+        <p>默认卡片内容</p>
+      </slot> -->
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title:{
+                type: String,
+                default: '默认标题'
+            }
+        },  
+    }
+</script>
+
+<style scoped>
+    .card {
+        border: 1px solid hsl(250deg, 100%, 80%);
+        padding: 20px;
+        border-radius: 8px;
+    }
+    .title {
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-bottom: 12px;
+    }
+</style>
