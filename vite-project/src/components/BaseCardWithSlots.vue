62608b846065a4fb851990fd0debe9027054ac79
JayHung <youngjay9@gmail.com>
2024-05-29 13:11:05 +0800

    新增 component: BaseCardWithSlots, 提供多個 slots 的功能.

---
+++ vite-project/src/components/BaseCardWithSlots.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <div class="content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+           
+        },  
+    }
+</script>
+
+<style scoped>
+    .card {
+        border: 1px solid hsl(250deg, 100%, 80%);
+        padding: 20px;
+        border-radius: 8px;
+    }
+    .title {
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-bottom: 12px;
+    }
+</style>
